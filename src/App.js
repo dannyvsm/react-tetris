45075d080bcff78e38df16906990a4a1bcc30bcd
lou duncan <31244585+dannyvsm@users.noreply.github.com>
2021-09-23 14:38:39 +0100

    Created components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Tetris from './components/Tetris';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Tetris />
+  </div>
+);
 
 export default App;
